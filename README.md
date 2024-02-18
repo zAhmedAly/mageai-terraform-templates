e920304209e9e6571c630b5fb56f9824ca0bfa29
Ahmed Aly <aaoflaca@gmail.com>
2024-02-18 13:28:46 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# mage-ai-terraform-templates
+Terraform templates for deploying mage-ai to AWS, GCP and Azure
+# mageai-terraform-templates
---
+++ gcp/variables.tf
@@ -0,0 +1,71 @@
+variable "app_name" {
+  type        = string
+  description = "Application Name"
+  default     = "mageapp"
+}
+
+variable "container_cpu" {
+  description = "Container cpu"
+  default     = "2000m"
+}
+
+variable "container_memory" {
+  description = "Container memory"
+  default     = "2G"
+}
+
+variable "project_id" {
+  type        = string
+  description = "The name of the project"
+  default     = "dtc-de-course-01152024"
+}
+
+variable "region" {
+  type        = string
+  description = "The default compute region"
+  default     = "us-west2"
+}
+
+variable "zone" {
+  type        = string
+  description = "The default compute zone"
+  default     = "us-west2-a"
+}
+
+variable "repository" {
+  type        = string
+  description = "The name of the Artifact Registry repository to be created"
+  default     = "mageapp"
+}
+
+variable "database_user" {
+  type        = string
+  description = "The username of the Postgres database."
+  default     = "root"
+}
+
+variable "database_password" {
+  type        = string
+  description = "The password of the Postgres database."
+  sensitive   = true
+  default     = "root"
+
+}
+
+variable "docker_image" {
+  type        = string
+  description = "The docker image to deploy to Cloud Run."
+  default     = "mageai/mageai:latest"
+}
+
+variable "domain" {
+  description = "Domain name to run the load balancer on. Used if `ssl` is `true`."
+  type        = string
+  default     = ""
+}
+
+variable "ssl" {
+  description = "Run load balancer on HTTPS and provision managed certificate with provided `domain`."
+  type        = bool
+  default     = false
+}
